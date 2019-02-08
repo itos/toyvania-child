b52b00a7a1ba6eac8767ae2007f7cbed0431064c
Carlos Oporto <carlosoporto@gmail.com>
2019-02-08 01:38:46 +0100

    remove WPBakery Page Builder generator meta

--- functions.php
+++ functions.php
@@ -1,16 +1,21 @@
-<?php 
+<?php
 
 add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles');
 function salient_child_enqueue_styles() {
-	
+
 		$nectar_theme_version = nectar_get_theme_version();
-		
+
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'), $nectar_theme_version);
 
-    if ( is_rtl() ) 
+    if ( is_rtl() )
    		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
 }
 
-
+add_action('wp_head', 'myoverride', 1);
+function myoverride() {
+  if ( class_exists( 'Vc_Manager' ) ) {
+    remove_action('wp_head', array(visual_composer(), 'addMetaData'));
+  }
+}
 
 ?>
