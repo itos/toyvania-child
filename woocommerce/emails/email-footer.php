0119db3cb54e637a68a70a652e1d33cc77210af5
Carlos Oporto <carlosoporto@gmail.com>
2019-03-06 18:22:00 +0100

    Added Email Footer

---
+++ woocommerce/emails/email-footer.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Email Footer
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/email-footer.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+															</div>
+														</td>
+													</tr>
+												</table>
+												<!-- End Content -->
+											</td>
+										</tr>
+									</table>
+									<!-- End Body -->
+								</td>
+							</tr>
+							<tr>
+								<td align="center" valign="top">
+									<!-- Footer -->
+									<table border="0" cellpadding="10" cellspacing="0" width="600" id="template_footer">
+										<tr>
+											<td valign="top">
+												<table border="0" cellpadding="10" cellspacing="0" width="100%">
+													<tr>
+															<!-- Start Custom Email Footer -->
+															<td style="font:14px/24px San Francisco,Segoe,Roboto,Arial,Helvetica,sans-serif;color:#333;padding:0 10px 0 10px;text-align:center;width:580px">
+										<table align="center" bgcolor="#fff" style="padding:5px 0 10px 0;width:580px;margin:0 auto;border-top:3px solid #ededed;text-align:center;border-radius:0 0 6px 6px" cellpadding="0" cellspacing="0">
+											<tbody>
+											<tr>
+												<td>
+													<p style="text-align:center;font-size:14px;color:#9da0a0">
+														Join the community
+													</p>
+													<p>
+														<a href="https://www.facebook.com/Toyvania/" target="_blank"><img src="https://toyvania.com/wp-content/uploads/2019/03/facebook-icon.png" style="margin:0 10px" class="CToWUd"></a>
+														<a href="https://www.instagram.com/toyvania/" target="_blank"><img src="https://toyvania.com/wp-content/uploads/2019/03/instagram-icon.png" class="CToWUd" style="margin:0 10px"></a>
+														<a href="https://twitter.com/toyvania" target="_blank"><img src="https://toyvania.com/wp-content/uploads/2019/03/twitter-icon.png" style="margin:0 10px" class="CToWUd"></a>
+													</p>
+													<p>
+														<a href="https://toyvania.com/my-account/" style="color:#ef4427" target="_blank" style="margin-right: 20px;">My Account</a>
+														<a href="https://toyvania.com/my-account/orders/" style="color:#ef4427" target="_blank">My Orders</a>
+													</p>
+												</td>
+											</tr>
+											<tr>
+												<td align="center" style="padding:10px 0 8px 0;font:17px/19px San Francisco,Segoe,Roboto,Arial,Helvetica,sans-serif;color:#333">
+													<span class="il">Toyvania</span>
+												</td>
+											</tr>
+											<tr>
+												<td align="center" style="padding:10px 0 0;font:13px/17px San Francisco,Segoe,Roboto,Arial,Helvetica,sans-serif;color:#ef4427">
+													<a style="color:#ef4427" href="https://toyvania.com" target="_blank">toyvania.com</a>
+												</td>
+											</tr>
+											</tbody>
+										</table>
+									</td>
+															<!-- End Custom Email Footer -->
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+									<!-- End Footer -->
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
